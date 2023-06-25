665446392eb11863fceb7cd37f103745ea215db8
Roman Parkhomenko <roman.s.parkhomenko@gmail.com>
2023-06-25 20:24:52 +0300

    end lesson. add to StarRating returnValueFunc

---
+++ src/components/UI/StarRating.jsx
@@ -0,0 +1,59 @@
+import Star from "./Star";
+import { useState } from "react";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+const starContainerStyle = {
+  display: "flex",
+  // gap: "4px",
+};
+const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  clasName = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  const [rating, setRating] = useState(defaultRating);
+  const [temporaryRating, setTemporaryRating] = useState(0);
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    fontSize: size / 1.5 + "px",
+    color,
+  };
+  const handleRate = (rating) => {
+    setRating(rating);
+    if (onSetRating) onSetRating(rating);
+  };
+
+  return (
+    <div style={containerStyle} className={clasName}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            isFull={temporaryRating ? temporaryRating > i : rating > i}
+            onRate={() => handleRate(i + 1)}
+            onHoverIn={() => setTemporaryRating(i + 1)}
+            onHoverOut={() => setTemporaryRating(0)}
+            size={size}
+            color={color}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[temporaryRating - 1] || messages[rating - 1] || ""
+          : temporaryRating || rating || ""}
+      </p>
+    </div>
+  );
+};
+
+export default StarRating;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StarRating from "./components/StarRating";
+import StarRating from "./components/UI/StarRating";
+import { average } from "./utils/average";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={10} />
+    <Test />
+    <StarRating
+      maxRating={10}
+      color="#f0f"
+      size={44}
+      clasName="test"
+      defaultRating={3}
+    />
+
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState([]);
+  const setMovieRatingHandler = (newRating) =>
+    setMovieRating((rating) => [...rating, newRating]);
+
+  const averageMovieRating = average(movieRating).toFixed(1);
+  return (
+    <>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetRating={setMovieRatingHandler}
+      />
+      <p>
+        {averageMovieRating > 0 ? "rating: " + averageMovieRating : "no rating"}
+      </p>
+    </>
+  );
+}
